3d4c64e405b85ceebd9f6a252a0f673f865ed00b
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2019-06-27 16:21:21 +0300

    Added neccessary scripts

---
+++ seed.js
@@ -0,0 +1,9 @@
+const submissions= [
+    {
+        id:1,
+        title:'This is the first Title',
+        description:'This is the description of the first submission.',
+        votes:16,
+        image:'assets/img/beach.jpg' 
+    }
+]
